5b82bab2753b452d7e9d67b1128d62cc47122b0f
Sumit Kumar <rsumit123@gmail.com>
2023-09-20 19:37:05 +0530

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header/Header';
 import SpeechComponent from './SpeechComponent/SpeechComponent';
 import "./App.css"
 
 const App = () => {
   return (
     <div className="app-container">
+      <Header/>
       <SpeechComponent />
     </div>
   );
---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      AI Health
+    </div>
+  );
+};
+
+export default Header;
